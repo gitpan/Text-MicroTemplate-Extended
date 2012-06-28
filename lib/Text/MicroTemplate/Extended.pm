9c22dd55689e81cf42dddc43aec0510e20fe4f82
Daisuke Murase <typester@gmail.com>
2012-06-28 02:25:45 +0000

    Import of TYPESTER/Text-MicroTemplate-Extended-0.14 from CPAN.
    gitpan-cpan-distribution: Text-MicroTemplate-Extended
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         TYPESTER/Text-MicroTemplate-Extended-0.14.tar.gz
    gitpan-cpan-author:       TYPESTER
    gitpan-cpan-maturity:     released

--- lib/Text/MicroTemplate/Extended.pm
+++ lib/Text/MicroTemplate/Extended.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base 'Text::MicroTemplate::File';
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 sub new {
     my $self = shift->SUPER::new(@_);
@@ -197,7 +197,7 @@ sub build {
     my $die_msg;
     {
         local $@;
-        if (my $builder = $self->eval_builder) {
+        if (my $builder = $self->eval_builder($offset)) {
             return $builder;
         }
         $die_msg = $self->_error($@, 2 + $offset, $context->{caller});
